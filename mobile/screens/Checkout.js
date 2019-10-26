9b3559e6648748b4d28da78c13bc1cb7a7dc9a32
manyaagarwal <manya18ag@gmail.com>
2019-10-27 06:41:53 +0800

    mobile: add Order

--- mobile/screens/Checkout.js
+++ mobile/screens/Checkout.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { View, Button, Text } from 'react-native';
 import CheckoutCard from '../components/CheckoutCard';
+import {addOrder} from "../store/actions/products";
 
-export default class Checkout extends React.Component {
+class Checkout extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            cartItem: {
-                id : 1,
+            cartItem: [{
+                id : '9gRo2xMlAwDsZt8IVZdi',
                 uri : 'https://www.fantasticfurniture.com.au/medias/BTOBEDDBLOOOFABCHA-LIF-CONTAINER-original-FantasticFurniture-WF-Product-Detail?context=bWFzdGVyfGltYWdlcy9CVE9CRUREQkxPT09GQUJDSEF8MTE4NTAwfGltYWdlL2pwZWd8aW1hZ2VzL0JUT0JFRERCTE9PT0ZBQkNIQS9oOGIvaDUwLzkwNjA4MjMzMDIxNzQuanBnfDAwOTZiZTA2OTU0MjI1NDBkZWM1MGVhNjBkZjYxZjdjYmRkMGFiMGVjM2I3ZmEwYjAxMmI1YjVkNzU2YTEyNzU',
                 name : 'Fancy King size bed',
                 rating : 3.1,
                 price : '$5,042 HKD'
-            }
+            }]
         }
     }
 
     render() {
+        let ids = this.state.cartItem.map(item => item.id);
         return(
             <View style={{ flex: 1 }}>
                 <Text style={{ fontSize: 24, marginTop: 20, marginLeft: 20 }}> Your Items </Text>
@@ -26,8 +28,19 @@ export default class Checkout extends React.Component {
                 <Button 
                     style={{ position: 'absolute', bottom: 0 }}
                     title="Checkout"
-                    onPress={() => Alert.alert('Simple Button pressed')} />
+                    onPress={() => {
+                        this.props.addOrder(ids);
+                        Alert.alert('Order place')
+                    }} />
             </View>
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return{
+        ...state,
+    }
+}
+
+export default connect(mapStateToProps, {addOrder})(Checkout)
