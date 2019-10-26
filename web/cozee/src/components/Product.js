a607ce58f9bc8bda5996f5d33e5d525390fd879d
uday1331 <u3555276@connect.hku.hk>
2019-10-27 01:35:42 +0800

    web: added styling

--- web/cozee/src/components/Product.js
+++ web/cozee/src/components/Product.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import {Card, Tag, Typography} from "antd";
+import React from "react";
+import { Card, Tag, Typography } from "antd";
 
-const {Meta} = Card;
+const { Meta } = Card;
 const { Text } = Typography;
 
-class Product extends React.Component{
-    render() {
-        const {product} = this.props;
-        const title = () => {
-            return (
-                <Text>{product.title} &nbsp; <Tag>{product.category}</Tag></Text>
-            )
+class Product extends React.Component {
+  render() {
+    const { product } = this.props;
+    const title = () => {
+      return (
+        <Text>
+          {product.title} &nbsp; <Tag>{product.category}</Tag>
+        </Text>
+      );
+    };
+    return (
+      <Card
+        hoverable
+        style={{ width: 240, margin: "5px", maxHeight: 300 }}
+        cover={
+          //image is always 240*150
+          <img
+            alt={product.title}
+            src={product.img}
+            style={{
+              height: "150px"
+            }}
+          />
         }
-        return(
-            <Card
-                hoverable
-                style={{ width: 240, margin:'5px' }}
-                cover={<img alt={product.title} src={product.img} />}
-            >
-                <Meta title={title()} description={product.description} />
-                <Text>Price: {product.price}</Text>
-            </Card>
-        )
-    }
+      >
+        <Meta title={title()} description={product.description} />
+        <Text>Price: {product.price}</Text>
+      </Card>
+    );
+  }
 }
 
 export default Product;
--- web/cozee/src/routes/index.js
+++ web/cozee/src/routes/index.js
@@ -6,6 +6,7 @@ import AppBar from "../components/AppBar";
 import Profile from "../components/Profile";
 import CreateProduct from "../components/CreateProduct";
 import ViewProduct from "../components/ViewProduct";
+import Sales from "../components/Sales";
 
 class Index extends React.Component {
   render() {
@@ -13,9 +14,11 @@ class Index extends React.Component {
       <BrowserRouter>
         <Switch>
           <AppBar>
+            <Route exact path="/" component={Profile} />
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/products/add" component={CreateProduct} />
-            <Route exact path="/products/view" component={ViewProduct}/>
+            <Route exact path="/products/view" component={ViewProduct} />
+            <Route exact path="/sales" component={Sales} />
           </AppBar>
         </Switch>
       </BrowserRouter>
