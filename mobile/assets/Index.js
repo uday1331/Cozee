38996f69c9439dfdc11e755c60ca3abdc28a22ca
ahsan8244 <syed.ahsan8244@gmail.com>
2019-10-27 06:44:54 +0800

    mobile: add cool 3d modelz

--- mobile/assets/Index.js
+++ mobile/assets/Index.js
@@ -1,5 +1,7 @@
 export default {
     "models": {
-        "stool": require("./Models/frehgstol.dae")
+        "stool": require("./Models/frehgstol.dae"),
+        "cupboard1": require("./Models/wrldfurfre.dae"),
+        "cupboard2": require("./Models/cupboard2.dae")
     }
 }
--- mobile/screens/marketplace/Furniture.js
+++ mobile/screens/marketplace/Furniture.js
@@ -27,7 +27,7 @@ export default class Furniture extends Component {
 
     tryInAR = () => {
         this.props.navigation.navigate('Design', {
-            selectedItem: Assets.models.stool
+            selectedItem: Assets.models.cupboard1
         });
     }
 
